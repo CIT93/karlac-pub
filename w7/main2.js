29948ab07e79444c4f02f578508c4ccf85ef85f1
Karla Clemmons <kclemms@hotmail.com>
2024-02-26 20:18:46 -0800

    TBL body my code

--- w7/main2.js
+++ w7/main2.js
@@ -1,6 +1,5 @@
 const FORM = document.getElementById("form");
 const OUTPUT = document.getElementById("output");
-const TBL = document.getElementById("tab-data");
 const cfpData = [];
 
 function determineHouseSizePts(size) {
@@ -43,7 +42,7 @@ function start(first, last, houseHoldMembers, houseSize) {
   const total = houseHoldPTS + houseSizePTS;
 cfpData.push({
   firstName: first,
-  lastName: last,
+  lastName: last, 
   houseM: houseHoldMembers,
   houseS: houseSize,
   houseMPTS: houseHoldPTS,
@@ -56,9 +55,9 @@ function displayOutput() {
 
   for (obj of cfpData) {
     const newH2 = document.createElement("h2");
-    newH2.textContent = `Carbon Footprint ${obj.cfpTotal}`;
+    newH2.textContent = `Cardon Footprint ${obj.cfpTotal}`;
     const newH3 = document.createElement("h3");
-    newH3.textContent = `Based on number in and size of home of ${obj.firstName} ${obj.lastName}`;
+    newH3.textContent = `Based on number in and size of home for ${obj.firstName} ${obj.lastName}`
     const newP = document.createElement("p");
     newP.textContent = `This number is based on the number of people in the house of ${obj.houseM} (score: ${obj.houseMPTS}),`;  
     newP.textContent += ` and a ${obj.houseS} size of home (score: ${obj.houseSPTS}).`; 
@@ -68,52 +67,6 @@ function displayOutput() {
   }
 }
 
-function renderTblHeading() {
-  const table = document.createElement("table");
-  const thead = document.createElement("thead");
-  const tr = document.createElement("tr");
-  const headingTextArr = ["Name", "HouseHold", "HouseSize", "Footprint", "Actions"]
-  headingTextArr.forEach(function(text){
-    const th = document.createElement("th");
-    th.textContent = text;
-    tr.appendChild(th);
-  });
-  thead.appendChild(tr);
-  table.appendChild(thead);
-  return table
-}
-
-function renderTbl(data) {
-const table =  renderTblHeading();
-const tbody = document.createElement("tbody");
-const tr = document.createElement("tr");
-const trTextArr = ["Kar", 3, "large", 20];
-trTextArr.forEach(function(text){
-  const td = document.createElement("td");
-  td.textContent = text;
-  tr.appendChild(td);
-})
-const td = document.createElement("td");
-const btnEdit = document.createElement("button");
-const btnDel = document.createElement("button");
-btnEdit.textContent = "Edit";
-btnDel.textContent = "Del";
-
-td.appendChild(btnEdit);
-td.appendChild(btnDel);
-tr.appendChild(td);
-tbody.appendChild(tr);
-table.appendChild(tbody);
-console.log(table);
-
-TBL.appendChild(table);
-}
-
-// My guess is the reason for the td error is because td was used
-// as a textContent and maybe buttons require more to be declared?
-
-
-
 FORM.addEventListener('submit', function(e){
   e.preventDefault();
   const firstName = FORM.firstname.value;
@@ -122,8 +75,6 @@ FORM.addEventListener('submit', function(e){
   const houseSize = FORM.houses.value;
   start(firstName, lastName, houseMembers, houseSize);
   OUTPUT.innerHTML = "";
- // displayOutput();
-  renderTbl(cfpData);
+  displayOutput();
   FORM.reset();
 })
-
